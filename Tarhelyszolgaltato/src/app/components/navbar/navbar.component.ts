a82c7f309f90e97888e4b10599bd3b8def68d484
vargajanos <janosvarga706@gmail.com>
2025-02-03 09:40:51 +0100

    login, regisztráció de nagyon ocsmány

--- Tarhelyszolgaltato/src/app/components/navbar/navbar.component.ts
+++ Tarhelyszolgaltato/src/app/components/navbar/navbar.component.ts
@@ -1,12 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { MenuItem } from 'primeng/api';
+import { MenubarModule } from 'primeng/menubar';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [MenubarModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
-export class NavbarComponent {
+export class NavbarComponent implements OnInit {
+  items: MenuItem[] | undefined;
 
+  ngOnInit() {
+    this.items = [
+        {
+            label: 'Login',
+            icon: 'pi pi-turkish-lira',
+            routerLink: '/login'
+        },
+        {
+            label: 'Regisztráció',
+            icon: 'pi pi-crown',
+            routerLink: "/register"
+        },
+
+    ]
+}
 }
